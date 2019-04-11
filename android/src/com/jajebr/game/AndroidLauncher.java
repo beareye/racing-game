02f8b1d00bb75c82112deee03be4ab60bb1e5ea2
bchan061 <bchan061@berkeley.edu>
2019-04-10 20:36:59 -0700

    Initial commit with libGDX starter template

---
+++ android/src/com/jajebr/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.jajebr.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.jajebr.game.RacingGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new RacingGame(), config);
+	}
+}
---
+++ desktop/src/com/jajebr/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jajebr.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jajebr.game.RacingGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RacingGame(), config);
+	}
+}
